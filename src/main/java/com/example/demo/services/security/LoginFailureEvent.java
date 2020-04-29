26eb6aa5706cbb96f660c16c64cbffee802f92c0
George <gtsianakas94@gmail.com>
2020-04-29 17:28:26 +0300

    LoginFailure Commit
    Publish and Consume Spring Custom Events example

---
+++ src/main/java/com/example/demo/services/security/LoginFailureEvent.java
@@ -0,0 +1,16 @@
+package com.example.demo.services.security;
+
+import org.springframework.context.ApplicationEvent;
+
+public class LoginFailureEvent extends ApplicationEvent {
+
+    /**
+     * Create a new ApplicationEvent.
+     *
+     * @param source the object on which the event initially occurred (never {@code null})
+     */
+    public LoginFailureEvent(Object source) {
+        super(source);
+    }
+
+}
---
+++ src/main/java/com/example/demo/services/security/LoginFailureEventHandler.java
@@ -0,0 +1,17 @@
+package com.example.demo.services.security;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginFailureEventHandler implements ApplicationListener<LoginFailureEvent> {
+
+    @Override
+    public void onApplicationEvent(LoginFailureEvent event) {
+
+        Authentication authentication = (Authentication) event.getSource();
+        System.out.println("LoginEvent Failure for: " + authentication.getPrincipal());
+    }
+
+}
